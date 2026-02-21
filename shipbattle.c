51f999f74368fb344f2cab7249353e5aecb30a8f
GeorgeOficial <georgeantonio420@gmail.com>
2026-02-21 03:12:22 +0000

    Nivel Intermediário concluído

--- shipbattle.c
+++ shipbattle.c
@@ -5,33 +5,37 @@ int main(){
 
 //Criar o mapa 10x10. A "água" do jogo, com matrizes
 
-//Criar dois navios representados pelo número 3 e inserir no 
+//Criar dois navios representados pelo número 3 e inserir no
 //mapa com comprimento 3, um vertical e o outro horizontal
 
-int map[10][10];
+//Criar dois navios diagonais
 
-//Esta é a 'ÁGUA'
-for (int i=0; i<10;i++){
-    for(int j=0; j<10; j++){
-        map[i][j] = 0;
-    }
+int map[10][10] = {0};
+int linha;
+int coluna;
+
+for (int i=0; i<3;i++){
+//Acrescenta NAVIOS
+    //VERTICAL
+    linha=0; coluna=0;
+    map[linha+i][coluna]=3;
+    //HORIZONTAL
+    linha=4; coluna=7;
+    map[linha][coluna+i]=3;
+
+    //DIAGONAIS
+    linha=2; coluna=2;
+    map[linha+i][coluna+i]=3;
+
+    linha=7; coluna=5;
+    map[linha+i][coluna+i]=3;
 }
 
-//Acrescenta NAVIOS
+//Esta é a 'ÁGUA'
 
-//Vertical
-map[0][9] = 3;
-map[1][9] = 3;
-map[2][9] = 3;
-
-//Horizontal
-map[6][3] = 3;
-map[6][4] = 3;
-map[6][5] = 3;
-
-for (int i=0; i<10; i++){
+for(int i=0; i<10; i++){
     for(int j=0; j<10; j++){
-        printf("%d", map[i][j]);
+        printf("%d ", map[i][j]);
     }
     printf("\n");
 }
